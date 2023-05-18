2b14271a4eb5d4e14cf9b40a1c62941e292ebb10
BoskoJDev <117722529+BoskoJDev@users.noreply.github.com>
2023-05-18 17:24:47 +0200

    async

---
+++ WPFUserInterface/DemoMethods.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPFUserInterface
+{
+    public static class DemoMethods
+    {
+        public static List<string> PrepData()
+        {
+            List<string> output = new List<string>()
+            {
+                "https://www.yahoo.com",
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.cnn.com",
+                "https://www.amazon.com",
+                "https://www.facebook.com",
+                "https://www.twitter.com",
+                "https://www.codeproject.com",
+                "https://en.wikipedia.org/wiki/.NET_Framework"
+            };
+
+            return output;
+        }
+
+        public static List<WebsiteDataModel> RunDownloadSync()
+        {
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            foreach (string site in PrepData())
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                output.Add(results);
+            }
+
+            return output;
+        }
+
+        public static List<WebsiteDataModel> RunDownloadParallelSync()
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+
+            Parallel.ForEach(websites, (site) =>
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                output.Add(results);
+            });
+
+            return output;
+        }
+
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsyncV2(IProgress<ProgressReportModel> progress)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(websites, (site) =>
+                {
+                    WebsiteDataModel results = DownloadWebsite(site);
+                    output.Add(results);
+
+                    report.SitesDownloaded = output;
+                    progress.Report(report);
+                });
+            });
+
+            return output;
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadAsync(IProgress<ProgressReportModel> progress, CancellationToken cancellationToken)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = await DownloadWebsiteAsync(site);
+                output.Add(results);
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                report.SitesDownloaded = output;
+                progress.Report(report);
+            }
+
+            return output;
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsync()
+        {
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (string site in PrepData())
+                tasks.Add(DownloadWebsiteAsync(site));
+
+            return new List<WebsiteDataModel>(await Task.WhenAll(tasks));
+        }
+
+        private static async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel()
+            {
+                WebsiteUrl = websiteURL,
+                WebsiteData = await new WebClient().DownloadStringTaskAsync(websiteURL)
+            };
+
+            return output;
+        }
+
+        private static WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+    }
+}
---
+++ WPFUserInterface/ProgressReportModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WPFUserInterface
+{
+    public class ProgressReportModel
+    {
+        public List<WebsiteDataModel> SitesDownloaded { get; set; } = new List<WebsiteDataModel>();
+    }
+}
